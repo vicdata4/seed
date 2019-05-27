96d582c7e32426439ff0bbc401389bcd90263fcb
victorgh <victor.gonzalez@soprasteria.com>
2019-05-27 16:45:32 +0200

    feat: final

--- rollup.config.js
+++ rollup.config.js
@@ -23,7 +23,7 @@ export default {
         }),
         uglify(),
         copy({
-            targets: ['src/assets', './index.html'],
+            targets: ['src/assets', 'src/index.html'],
             outputFolder: 'build'
         }),
         serve({
