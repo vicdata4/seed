6429aad922c6ec94e0591947af7f1c287ff6dd68
victorgh <victor.gonzalez@soprasteria.com>
2019-10-19 22:45:45 +0200

    seed-modal

--- src/views/catalog-view.js
+++ src/views/catalog-view.js
@@ -1,6 +1,7 @@
 import { LitElement, html, css } from 'lit-element';
 import { CustomStyles, ViewStyle } from '../utils/custom-styles';
 import { seedButtonStyle } from 'seed-catalog/styles.js';
+import 'seed-catalog/components/seed-modal.js';
 
 class CatalogView extends LitElement {
   static get styles() {
@@ -23,8 +24,39 @@ class CatalogView extends LitElement {
           <hr>
           <h3>Web components catalog</h3>
           <p>Seed-catalog is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS- and (optionally) JavaScript-based design templates for typography, forms, buttons, navigation and other interface components.</p>
-          <p>Seed-catalog can be used either through a command line interface with an optional configuration file, or else through its JavaScript API. Run rollup --help to see the available options and parameters.</p>
-          <a href="https://rollupjs.org" target="_blank" rel="noopener" class="custom-link">Login</a>
+          <div>
+          <a href="https://seed-catalog.web.app" target="_blank" class="sd-btn blue">StoryBook</a>
+          <seed-modal>
+          <button slot="button" class="sd-btn-mix red"><i class="material-icons">people</i>Open modal</button>
+          <span slot="title">Building Web Components</span>
+          <button slot="close-btn" class="sd-btn-empty"><i class="material-icons blue-mate">close</i></button>
+          
+          <div slot="content">
+            Duis sit amet sollicitudin nulla, sit amet molestie magna. Aenean interdum justo id iaculis hendrerit. Nullam sed tellus pretium, mattis dui vitae, placerat diam. <br><br>Nulla faucibus lacus sed mauris facilisis, a condimentum risus lobortis. Nunc egestas vehicula libero, non condimentum enim semper in. Donec efficitur convallis felis nec malesuada. Morbi ornare massa diam, ac ultrices magna suscipit a. Integer tempus sagittis ligula, ut fermentum magna suscipit a. Sed pellentesque consequat velit, ut convallis tellus. Mauris eget felis tortor. Aliquam sed consectetur ipsum. Praesent vel tortor consectetur sem mollis tincidunt vitae nec purus. Nullam ac scelerisque est, vitae iaculis magna. Ut pellentesque, enim lobortis pellentesque egestas, libero nibh sodales sapien, eu dignissim tortor massa id neque. Ut at dui viverra, efficitur lectus sed, sagittis risus.
+          </div>
+          <div slot="footer">
+            <button class="sd-btn black">Close</button>
+            <button class="sd-btn blue-mate">Send</button>
+          </div> 
+        </seed-modal>
+
+        <seed-modal>
+          <button slot="button" class="sd-btn-mix blue-mate"><i class="material-icons">lock</i>Open modal</button>
+          <span slot="title">Building Web Components</span>
+          <button slot="close-btn" class="sd-btn-empty"><i class="material-icons blue-mate">close</i></button>
+          
+          <div slot="content">
+            Ut pellentesque, enim lobortis pellentesque egestas, libero nibh sodales sapien, eu dignissim tortor massa id neque. Ut at dui viverra, efficitur lectus sed, sagittis risus.
+          </div>
+          <div slot="footer">
+            <button class="sd-btn green-mate">Ok</button>
+            <button class="sd-btn red-mate">Deny</button>
+          </div> 
+        </seed-modal>
+        <button class="sd-btn-mix green"><i class="material-icons">folder</i>Success</button>
+        </div>
+        <div>
+        </div>
       </section>
     `;
   }
