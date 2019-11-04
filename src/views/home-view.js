75f6b53590124a982fb1d2ff9d6960255a9263da
victorgh <victor.gonzalez@soprasteria.com>
2019-11-04 19:59:46 +0100

    small changes

--- src/views/home-view.js
+++ src/views/home-view.js
@@ -43,39 +43,37 @@ class HomeView extends LitElement {
   render() {
     return html`
       <section>
-        <h1>${locales.home_title[0]} <span>${locales.home_title[1]}</span></h1>
+        <h1>
+          ${locales.home_title[0]}
+          <span>${locales.home_title[1]}</span>
+        </h1>
         <h1>${locales.home_title_sub}</h1>
         <hr>
         <p>${locales.subtitle}</p>
         <div class="links">
-          <a
-            href="/dependencies"
-            @click="${() => this.setActiveButton('/dependencies')}"
-            class="sd-btn blue-mate"
-            rel="noopener">
-            ${locales.home_link_dependencies}
+          <a href="/dependencies"
+             class="sd-btn blue-mate"
+             @click="${() => this.setActiveButton('/dependencies')}"
+             rel="noopener">
+             ${locales.home_link_dependencies}
           </a>
-          <a
-            href="/catalog"
-            @click="${() => this.setActiveButton('/catalog')}"
-            class="sd-btn-mix blue"
-            rel="noopener">
-            <i class="material-icons">import_contacts</i>
-            ${locales.home_link_catalog}
+          <a href="/catalog"
+             class="sd-btn-mix blue"
+             @click="${() => this.setActiveButton('/catalog')}"
+             rel="noopener">
+             <i class="material-icons">import_contacts</i>
+             ${locales.home_link_catalog}
           </a>
-          <a
-            href="/application"
-            @click="${() => this.setActiveButton('/application')}"
-            class="sd-btn red-mate"
-            rel="noopener">
-            ${locales.home_link_auth}
+          <a href="/application"
+             class="sd-btn red-mate"
+             @click="${() => this.setActiveButton('/application')}"
+             rel="noopener">
+             ${locales.home_link_auth}
           </a>
-          <a
-            href="https://github.com/vicdata4/seed"
-            class="sd-btn-mix purple"
-            rel="noopener">
-            <i class="material-icons">cloud_queue</i>
-            Github
+          <a href="https://github.com/vicdata4/seed"
+             class="sd-btn-mix purple"
+             rel="noopener">
+             <i class="material-icons">cloud_queue</i>Github
           </a>
         </div>
         <language-selector></language-selector>
