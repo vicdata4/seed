46ba4c729649dc291150861ad71bf48c0eba15ff
vicdata <vicdata@outlook.com>
2020-05-06 22:57:35 +0200

    Updated seed-modal slots

--- src/views/catalog-view.js
+++ src/views/catalog-view.js
@@ -18,6 +18,22 @@ class CatalogView extends LitElement {
         button {
           margin-bottom: 7px;
         }
+
+        .header {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          border-bottom: 1px solid #dedede;
+        }
+
+        .footer {
+          display: flex;
+          justify-content: flex-end;
+        }
+
+        .footer button {
+          margin-left: 5px;
+        }
       `
     ];
   }
@@ -33,27 +49,31 @@ class CatalogView extends LitElement {
 
         <seed-modal>
           <button slot="button" class="sd-btn-mix red"><i class="material-icons">tv</i>Default modal</button>
-          <span slot="title">${locales.catalog_modal_title}</span>
-          <button slot="close-btn" class="sd-icon clear"><i class="material-icons blue-mate">close</i></button>
-        
+          <div slot="header" class="header">      
+            <span>${locales.catalog_modal_title}</span>
+            <button class="sd-icon clear close"><i class="material-icons blue-mate">close</i></button>
+          </div>
+
           <div slot="content">
             Duis sit amet sollicitudin nulla, sit amet molestie magna. Aenean interdum justo id iaculis hendrerit. Nullam sed tellus pretium, mattis dui vitae, placerat diam. <br><br>Nulla faucibus lacus sed mauris facilisis, a condimentum risus lobortis. Nunc egestas vehicula libero, non condimentum enim semper in. Donec efficitur convallis felis nec malesuada. Morbi ornare massa diam, ac ultrices magna suscipit a. Integer tempus sagittis ligula, ut fermentum magna suscipit a. Sed pellentesque consequat velit, ut convallis tellus. Mauris eget felis tortor. Aliquam sed consectetur ipsum. Praesent vel tortor consectetur sem mollis tincidunt vitae nec purus.
           </div>
-          <div slot="footer">
-            <button class="sd-btn black">Close</button>
+          <div slot="footer" class="footer">
+            <button class="sd-btn black close">Close</button>
             <button class="sd-btn blue-mate">Send</button>
           </div> 
         </seed-modal>
 
-        <seed-modal alignWindow="center">
+        <seed-modal centered>
           <button slot="button" class="sd-btn-mix blue-mate"><i class="material-icons">info</i>Centered modal</button>
-          <span slot="title">${locales.catalog_modal_title}</span>
-          <button slot="close-btn" class="sd-icon clear"><i class="material-icons green-mate">close</i></button>
+          <div slot="header" class="header">      
+            <span>${locales.catalog_modal_title}</span>
+            <button class="sd-icon clear close"><i class="material-icons blue-mate">close</i></button>
+          </div>
           
           <div slot="content">
             Ut pellentesque, enim lobortis pellentesque egestas, libero nibh sodales sapien, eu dignissim tortor massa id neque. Ut at dui viverra, efficitur lectus sed, sagittis risus.
           </div>
-          <div slot="footer">
+          <div slot="footer" class="footer">
             <button class="sd-btn green-mate">Ok</button>
             <button class="sd-btn red-mate">Deny</button>
           </div> 
