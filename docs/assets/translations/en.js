42d533c87c619196d2cd6f0a8febba3342e21f22
vicdata <vicdata@outlook.com>
2022-02-12 14:25:54 +0100

    chore: added docs

---
+++ docs/assets/translations/en.js
@@ -0,0 +1,30 @@
+export default {
+  references_title: 'References',
+
+  catalog_subtitle: 'Web components library',
+  catalog_text: 'Seed-catalog is a free and open-source web-components library. It contains CSS-styles and standard-web based templates such as buttons, modals, dropdowns and other interface components.',
+  catalog_modal_title: 'Building Web Components',
+
+  github: 'GitHub repository',
+
+  home_title: ['Lightweight', 'seed'],
+  home_title_sub: 'based on web components',
+  home_link_dependencies: 'Dependencies',
+  home_link_catalog: 'Catalog',
+  home_link_auth: 'Auth',
+
+  language: 'Language',
+  login_title: 'Sing up',
+  login_password: 'password',
+  login_submit: 'Login',
+  login_logout: 'Logout',
+
+  nav_home: 'Home',
+  nav_login: 'Login',
+
+  page_not_found: 'Page not found',
+  page_not_found_text: 'Go to Home page',
+
+  quick_start: 'Quick start',
+  subtitle: 'Learn about RollUp, LitElement, Redux and Vaadin'
+};
